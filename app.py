0b25ca2a286aa0bcf64a6b4a5dd636b2a8a9a371
sangkyulee@gmail.com <sangkyulee@gmail.com>
2022-05-15 21:10:21 +0000

    clean up old code 	deleted:    .pre-commit-config.yaml 	modified:   app.py 	deleted:    app1.py 	deleted:    app3.py 	deleted:    app_tb.py 	deleted:    pose_app/fo.py 	deleted:    pose_app/tb.py 	deleted:    pose_app/train.py 	deleted:    quick_start/.gridignore 	deleted:    quick_start/__init__.py 	deleted:    quick_start/components.py 	deleted:    quick_start/train/__init__.py 	deleted:    quick_start/train/demo_weights 	deleted:    quick_start/train/net.py 	deleted:    quick_start/train/train.py 	modified:   requirements.txt 	deleted:    run_fo.py 	deleted:    run_tb.py 	deleted:    script_args_editconfig.py 	deleted:    scripts/train_hydra.py 	deleted:    train_lp.py 	lai_components/select_fo_dataset.py

--- app.py
+++ app.py
@@ -1,52 +1,93 @@
+# app.py
 import os
+import lightning as L
+import streamlit as st
+from lai_components.run_ui import ScriptRunUI
+from lai_components.chdir_script import ChdirPythonScript
+from lai_components.run_tb import RunTensorboard
+from lai_components.select_fo_dataset import RunFiftyone, SelectDatasetUI
 import logging
-from typing import Any, Dict, Optional, Union
-from lightning import CloudCompute, LightningApp, LightningFlow, LightningWork
-from pose_app import *
+import time
 
-class App(LightningFlow):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.ui_train = TrainUI()
-    self.work_train = TrainScript()
-    self.work_tb = CreateTensorboard()
-    self.ui_fo = CreateFiftyoneUI()
-    self.work_fo = CreateFiftyoneDataset()
+# data.data_dir=./lightning-pose/toy_datasets/toymouseRunningData 
+# Saved predictions to: pred_csv_files_to_plot=/home/jovyan/lightning-pose-app/lightning-pose/outputs/2022-05-15/16-06-45/predictions.csv
+#             pred_csv_files_to_plot=["./lightning-pose/outputs/2022-05-15/16-06-45/predictions.csv"]  
+#            test_videos_directory="./lightning-pose/toy_datasets/toymouseRunningData/unlabeled_videos" \##
+#            saved_vid_preds_dir="./lightning-pose/toy_datasets/toymouseRunningData" \
+#            video_file_to_plot="./lightning-pose/toy_datasets/toymouseRunningData/unlabeled_videos/test_vid.mp4" \
 
-  def run(self):
-    self.ui_train.run()
-    self.ui_tb.run()
-    # train
-    self.ui_train.st_submit:
-      self.ui_train.st_submit = False
-      self.work_train.run(
-        script_path=os.path.join(self.ui_train.st_script_dir, self.ui_train.st_script_name),
-        script_args=self.ui_train.st_script_arg,
-        script_env=self.ui_train.st_script_env)
-    # fo      
-    self.ui_fo.run()
-    self.ui_fo.st_submit:
-      self.ui_fo.st_submit = False
-      self.work_fo.run(
-        script_path=os.path.join(self.ui_fo.st_script_dir, self.ui_fo.st_script_name),
-        script_args=self.ui_fo.st_script_arg,
-        script_env=self.ui_fo.st_script_env)
+class LitPoseApp(L.LightningFlow):
+    def __init__(self):
+        super().__init__()
+        self.dataset_ui = SelectDatasetUI()
+        self.train_ui = ScriptRunUI(
+          script_dir = "./lightning-pose",
+          script_name = "scripts/train_hydra.py",
+          script_env = "HYDRA_FULL_ERROR=1",
+          config_dir = "./lightning-pose/scripts/configs",
+          config_ext = "*.yaml",        
+          script_args = """training.max_epochs=11 
+model.losses_to_use=[] 
+          """
+        )
+        self.fo_ui = ScriptRunUI(
+          script_dir = "./lightning-pose",
+          script_name = "scripts/create_fiftyone_dataset.py",
+          script_env = "HYDRA_FULL_ERROR=1",
+          config_dir = "./lightning-pose/scripts/configs",
+          config_ext = "*.yaml",        
+          script_args = """eval.fiftyone.dataset_name=test1 
+eval.fiftyone.model_display_names=["test1"]
+eval.hydra_paths=['2022-05-15/16-06-45']
+eval.fiftyone.address=${host} 
+eval.fiftyone.port=${port} 
+eval.fiftyone.dataset_to_create="images"
+eval.fiftyone.build_speed="fast" 
+eval.fiftyone.launch_app_from_script=False 
+            """  
+        )        
+        self.run_tb = RunTensorboard(logdir = "./lightning-pose/outputs", blocking=False, run_once=True)
+
+        # script_path is required at init, but will be override in the run
+        self.dataset_runner = ChdirPythonScript("app.py",blocking=True,run_once=False)   
+        self.train_runner = ChdirPythonScript("app.py",blocking=True,run_once=False)
+        self.fo_runner = RunFiftyone(blocking=True,run_once=True)
+
+    def run(self):
+      self.run_tb.run()
+
+      if self.dataset_ui.st_submit:      
+        self.dataset_ui.st_submit = False
+        print(f"st_selectbox={self.dataset_ui.st_selectbox}")
+        #time.sleep(10) # runs too fast will come in here twice
+        if not(self.dataset_ui.st_selectbox is None):
+          self.fo_runner.run(dataset_name = self.dataset_ui.st_selectbox)
+
+      # create new dataset
+      if self.fo_ui.st_submit:      
+        self.fo_ui.st_submit = False
+        self.dataset_runner.run(root_dir = self.fo_ui.st_script_dir, 
+          script_name = self.fo_ui.st_script_name, 
+          script_args=self.fo_ui.st_script_args,
+          script_env=self.train_ui.st_script_env,
+          )
+        self.dataset_ui.set_dateset_names()
+
+      if self.train_ui.st_submit:      
+        self.train_ui.st_submit = False
+        self.train_runner.run(root_dir = self.train_ui.st_script_dir, 
+          script_name = self.train_ui.st_script_name, 
+          script_args=self.train_ui.st_script_args,
+          script_env=self.train_ui.st_script_env,
+          )
 
     def configure_layout(self):
-        tab1 = {"name": "Lightning Pose Param", "content": self.ui_train,}
-        tab2 = {"name": "Fiftyone Dataset Param", "content": self.ui_fo,}
+        tab1 = {"name": "Train", "content": self.train_ui}
+        tab2 = {"name": "Dataset", "content": self.fo_ui}
+        tab3 = {"name": "Tensorboard", "content": self.run_tb}
+        tab4 = {"name": "Pick Dataset", "content": self.dataset_ui}
+        tab5 = {"name": "Fiftyone", "content": self.fo_runner}
+        return [tab1, tab2, tab3, tab4, tab5]
 
-        if self.work_fo.has_started:
-            tab3 = { "name": "Fiftyone","content": self.work_fo,}
-        else:
-            tab3 = {"name": "Fiftyone", "content": ""}
-
-        if self.work_tb.has_started:
-            tab4 = {"name": "Tensorboard", "content": self.work_tb,}
-        else:
-            tab5 = {"name": "Tensorboard", "content": ""}
-
-        return [tab1, tab2, tab3, tab4]
-
-
-app = LightningApp(App())
+logging.basicConfig(level=logging.DEBUG)
+app = L.LightningApp(LitPoseApp())
