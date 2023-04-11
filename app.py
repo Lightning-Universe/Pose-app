7740c183a16c6a53e6e9ded00da379f83e2ff3d6
themattinthehatt <themattinthehatt@gmail.com>
2023-04-11 12:01:15 -0400

    update inference

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ class LitPoseApp(LightningFlow):
     def __init__(self):
         super().__init__()
 
-        # shared data for apps
+        # shared data for apps; NOTE: this is hard-coded in the run_inference method below too
         drive_name = "lit://lpa"
 
         # -----------------------------
@@ -179,19 +179,26 @@ class LitPoseApp(LightningFlow):
         for video in videos:
             self.inference[video] = LitPose(
                 cloud_compute=CloudCompute("gpu"),
-                drive_name=drive_name,
-                parallel=is_running_in_cloud(),
+                drive_name="lit://lpa",  # see note below
+                parallel=True,  # is_running_in_cloud(),
             )
-            print(f"launching inference for video {video}")
             self.inference[video].run('run_inference', model=model, video=video)
 
         # clean up works
         while len(self.inference) > 0:
-            for vid, work in self.inference.items():
-                if w.work.status.stage == "succeeded":
+            for video in list(self.inference):
+                if video in self.inference.keys() and self.inference[video].work_is_done_inference:
                     # kill work
                     print(f"killing work from video {video}")
-                    del self.inference[vid]
+                    del self.inference[video]
+
+        print("--------------------")
+        print("END OF INFERENCE")
+        print("--------------------")
+
+        # drive_name has to be hard-coded because if we save it as an attribute of the app it
+        # automatically gets cast into a lightning Path object; however, you cannot pass a Path
+        # object to Drive without an error
 
     def run(self):
 
@@ -307,7 +314,6 @@ class LitPoseApp(LightningFlow):
             if self.litpose.work_is_done_extract_frames:
                 self.project_io.run(action="update_frame_shapes")
                 self.label_studio.run(action="update_tasks", videos=self.extract_ui.st_video_files)
-                self.litpose.work_is_done_extract_frames = False
                 self.extract_ui.run_script = False
 
         # -------------------------------------------------------------
--- lightning_pose_app/litpose.py
+++ lightning_pose_app/litpose.py
@@ -12,6 +12,8 @@ class LitPose(LightningFlow):
         *args,
         cloud_compute,
         drive_name,
+        component_name="litpose",
+        parallel=False,
         **kwargs
     ) -> None:
         super().__init__(*args, **kwargs)
@@ -20,14 +22,20 @@ class LitPose(LightningFlow):
             cloud_compute=cloud_compute,
             cloud_build_config=LitPoseBuildConfig(),  # this is where Lightning Pose is installed
             drive_name=drive_name,
-            component_name="litpose",
+            component_name=component_name,
             wait_seconds_after_run=1,
+            parallel=parallel,
         )
 
-        self.work_is_done_extract_frames = False
+        self.work_is_done_extract_frames = True
         self.work_is_done_training = True
+        self.work_is_done_inference = True
+        self.count = 0
 
     def start_extract_frames(self, video_files=None, proj_dir=None, n_frames_per_video=20):
+
+        print(f"launching extraction for video {video_files[0]}")
+        self.work_is_done_extract_frames = False
 
         # set videos to select frames from
         vid_file_args = ""
@@ -54,7 +62,11 @@ class LitPose(LightningFlow):
         self.work_is_done_extract_frames = True
 
     def run_inference(self, model, video):
-        pass
+        import time
+        self.work_is_done_inference = False
+        print(f"launching inference for video {video} using model {model}")
+        time.sleep(5)
+        self.work_is_done_inference = True
 
     def run(self, action=None, **kwargs):
 
