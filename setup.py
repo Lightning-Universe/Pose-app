11a7d5fe55e079dafc1338fc1be8d6d7ea89552f
Matt Whiteway <themattinthehatt@gmail.com>
2023-11-30 19:42:56 +0000

    update litpose commit

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 from setuptools import find_packages, setup
 
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 
 # add the README.md file to the long_description
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 install_requires = [
-    "lightning[app]==2.1.0",
+    "lightning[app]",
     "numpy",
     "opencv-python",
     "pandas",
