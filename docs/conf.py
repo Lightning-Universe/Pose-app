d576bab0c63f16b8bcf5c162bdb556a45f47bfa3
themattinthehatt <themattinthehatt@gmail.com>
2023-12-01 12:03:01 -0500

    [docs] enable dark mode

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ extensions = [
     'sphinx.ext.napoleon',  # parsing of different docstring styles
     'sphinx_automodapi.automodapi',
     'sphinx_copybutton',  # add copy button to code blocks
+    'sphinx_rtd_dark_mode',
 ]
 
 templates_path = ['_templates']
