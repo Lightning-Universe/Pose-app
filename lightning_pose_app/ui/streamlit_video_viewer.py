d9933ee9497b825fdace82f245c5242373d7505f
Shmuel-columbia <ss7153@columbia.edu>
2024-05-15 19:58:05 +0000

    strealine and optimize functions in video player tab

--- lightning_pose_app/ui/streamlit_video_viewer.py
+++ lightning_pose_app/ui/streamlit_video_viewer.py
@@ -28,48 +28,43 @@ class StreamlitVideoViewer(LightningFlow):
 
 
 def get_models(model_dir):
-
+    """Return a list of all models."""
     trained_models = []
-    # this returns a list of model training days
-    dirs_day = os.listdir(model_dir)
-    # loop over days and find HH-MM-SS
-    for dir_day in dirs_day:
-        fullpath1 = os.path.join(model_dir, dir_day)
-        dirs_time = os.listdir(fullpath1)
-        for dir_time in dirs_time:
-            fullpath2 = os.path.join(fullpath1, dir_time)
-            trained_models.append('/'.join(fullpath2.split('/')[-2:]))
+    for dir_day in os.listdir(model_dir):
+        day_path = os.path.join(model_dir, dir_day)
+        for dir_time in os.listdir(day_path):
+            time_path = os.path.join(day_path, dir_time)
+            trained_models.append('/'.join(time_path.split('/')[-2:]))
     return trained_models
 
 
 def list_labeled_mp4_files(model_dir, selected_model):
     """List labeled MP4 files in a directory."""
-    labeled_mp4_files = []
-    if selected_model:
-        model_path = os.path.join(model_dir, selected_model)
-        for root, dirs, files in os.walk(model_path):
-            for file in files:
-                if file.endswith(".labeled.mp4"):
-                    labeled_mp4_files.append(os.path.join(root, file))
-    return labeled_mp4_files
+    if not selected_model:
+        return []
+    model_path = os.path.join(model_dir, selected_model)
+    return [
+        os.path.join(root, file)
+        for root, _, files in os.walk(model_path)
+        for file in files if file.endswith(".labeled.mp4")
+    ]
 
 
 def extract_video_name(full_path):
     """Extract the video name from a full file path."""
     base_filename = os.path.basename(full_path)  # Get the base filename
     video_name, _ = os.path.splitext(base_filename)  # Split filename and extension
-    return video_name
+    return video_name.replace('.labeled', '')  # Remove '.labeled' if present
 
 
 def list_results_files(video_name, model_dir, selected_model):
     """List results files for a given video."""
-    results_files = {}
     results_dir = os.path.join(model_dir, selected_model, MODEL_VIDEO_PREDS_INFER_DIR)
-    for file_name in os.listdir(results_dir):
-        if video_name in file_name and file_name.endswith(".csv"):
-            full_path = os.path.join(results_dir, file_name)
-            results_files[file_name] = full_path
-    return results_files
+    return {
+        file_name: os.path.join(results_dir, file_name)
+        for file_name in os.listdir(results_dir)
+        if video_name in file_name and file_name.endswith(".csv")
+    }
 
 
 def _render_streamlit_fn(state: AppState):
@@ -86,14 +81,10 @@ def _render_streamlit_fn(state: AppState):
     st.header("Visualize Model Predictions")
 
     with st.sidebar:
-
         st.write("Select the model and then the associated video you wish to inspect")
-
         trained_models = get_models(model_dir)  # create a list of all models
         selected_model = st.selectbox("Step 1: Select a model", trained_models)
-
         st.divider()
-
         # Labeled videos
         mp4_files = list_labeled_mp4_files(model_dir, selected_model)
         selected_video = st.selectbox(
@@ -113,16 +104,15 @@ def _render_streamlit_fn(state: AppState):
         </style>
         """
         st.markdown(custom_css, unsafe_allow_html=True)
-
         st.video(video_bytes)
         st.caption(
             "To download the video, click the three dots on the right side and select Download"
         )
+
         # Add a section to download results files
         st.header("Download Results Files")
         video_name = extract_video_name(selected_video)
-        video_name_org = video_name.replace('.labeled', '')
-        results_files = list_results_files(video_name_org, model_dir, selected_model)
+        results_files = list_results_files(video_name, model_dir, selected_model)
 
         if results_files:
             selected_result_files = st.multiselect(
@@ -131,7 +121,7 @@ def _render_streamlit_fn(state: AppState):
             if selected_result_files:
                 if len(selected_result_files) == 1:
                     selected_result_file = results_files[selected_result_files[0]]
-                    new_file_name = f"{selected_model}_{video_name_org}_{selected_result_files[0]}"
+                    new_file_name = f"{selected_model}_{video_name}_{selected_result_files[0]}"
                     with open(selected_result_file, "rb") as file:
                         st.download_button(
                             label="Download File",
@@ -148,11 +138,11 @@ def _render_streamlit_fn(state: AppState):
                     with zipfile.ZipFile(zip_buffer, "w") as zip_file:
                         for result_file_name in selected_result_files:
                             result_file_path = results_files[result_file_name]
-                            new_file_name = f"{selected_model}_{video_name_org}_{result_file_name}"
+                            new_file_name = f"{selected_model}_{video_name}_{result_file_name}"
                             with open(result_file_path, "rb") as file:
                                 zip_file.writestr(new_file_name, file.read())
                     zip_buffer.seek(0)
-                    zip_file_name = f"results_{selected_model}_{video_name_org}.zip"
+                    zip_file_name = f"results_{selected_model}_{video_name}.zip"
                     st.download_button(
                         label="Download Files",
                         data=zip_buffer,
