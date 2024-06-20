f87727f44067948d8e412933184b57794af38f64
Shmuel-columbia <ss7153@columbia.edu>
2024-06-20 20:11:11 +0000

    fix video viewer missing model error with basic ui page

--- lightning_pose_app/ui/streamlit_video_viewer.py
+++ lightning_pose_app/ui/streamlit_video_viewer.py
@@ -78,77 +78,84 @@ def _render_streamlit_fn(state: AppState):
     model_dir = abspath(os.path.join(proj_dir, MODELS_DIR))
 
     # Streamlit UI
-    st.header("Visualize Model Predictions")
-
-    with st.sidebar:
-        st.write("Select the model and then the associated video you wish to inspect")
-        trained_models = get_models(model_dir)  # create a list of all models
-        selected_model = st.selectbox("**Step 1:** Select a model", trained_models)
-        st.divider()
-        # Labeled videos
-        mp4_files = list_labeled_mp4_files(model_dir, selected_model)
-        selected_video = st.selectbox(
-            "**Step 2:** Select a video", mp4_files, format_func=extract_video_name,
-        )
-        st.divider()
-        # Add a section to download results files
-        video_name = extract_video_name(selected_video)
-        results_files = list_results_files(video_name, model_dir, selected_model)
-
-        if results_files:
-            selected_result_files = st.multiselect(
-                "**Step 3:** Select results files to download", list(results_files.keys())
+    if os.path.exists(model_dir):
+        st.header("Visualize Model Predictions")
+        with st.sidebar:
+            st.write("Select the model and then the associated video you wish to inspect")
+            trained_models = get_models(model_dir)  # create a list of all models
+            selected_model = st.selectbox("**Step 1:** Select a model", trained_models)
+            st.divider()
+            # Labeled videos
+            mp4_files = list_labeled_mp4_files(model_dir, selected_model)
+            selected_video = st.selectbox(
+                "**Step 2:** Select a video", mp4_files, format_func=extract_video_name,
             )
-            if selected_result_files:
-                if len(selected_result_files) == 1:
-                    selected_result_file = results_files[selected_result_files[0]]
-                    new_file_name = f"{selected_model}_{video_name}_{selected_result_files[0]}"
-                    with open(selected_result_file, "rb") as file:
-                        st.download_button(
-                            label="Download File",
-                            data=file,
-                            file_name=new_file_name
-                        )
-                else:
-                    st.warning(
-                        "If you select more than one file, they will be downloaded "
-                        "together as a ZIP folder"
-                    )
-                    # Create a zip file
-                    zip_buffer = BytesIO()
-                    with zipfile.ZipFile(zip_buffer, "w") as zip_file:
-                        for result_file_name in selected_result_files:
-                            result_file_path = results_files[result_file_name]
-                            new_file_name = f"{selected_model}_{video_name}_{result_file_name}"
-                            with open(result_file_path, "rb") as file:
-                                zip_file.writestr(new_file_name, file.read())
-                    zip_buffer.seek(0)
-                    zip_file_name = f"results_{selected_model}_{video_name}.zip"
-                    st.download_button(
-                        label="Download Files",
-                        data=zip_buffer,
-                        file_name=zip_file_name,
-                        mime="application/zip"
-                    )
-        else:
-            st.write("No results files available for this video.")
+            st.divider()
+            # Add a section to download results files
+            video_name = extract_video_name(selected_video)
+            results_files = list_results_files(video_name, model_dir, selected_model)
 
-    if selected_video:
-        # read and show the predictions labeled video
-        video_file = open(selected_video, "rb")
-        video_bytes = video_file.read()
-        custom_css = """
-        <style>
-        video {
-            width: 75% !important;
-            height: 50% !important;
-        }
-        </style>
-        """
-        st.markdown(custom_css, unsafe_allow_html=True)
-        st.video(video_bytes)
-        st.caption(
-            "To download the video, click the three dots on the right side and select Download"
-        )
+            if results_files:
+                selected_result_files = st.multiselect(
+                    "**Step 3:** Select results files to download", list(results_files.keys())
+                )
+                if selected_result_files:
+                    if len(selected_result_files) == 1:
+                        selected_result_file = results_files[selected_result_files[0]]
+                        new_file_name = f"{selected_model}_{video_name}_{selected_result_files[0]}"
+                        with open(selected_result_file, "rb") as file:
+                            st.download_button(
+                                label="Download File",
+                                data=file,
+                                file_name=new_file_name
+                            )
+                    else:
+                        st.warning(
+                            "If you select more than one file, they will be downloaded "
+                            "together as a ZIP folder"
+                        )
+                        # Create a zip file
+                        zip_buffer = BytesIO()
+                        with zipfile.ZipFile(zip_buffer, "w") as zip_file:
+                            for result_file_name in selected_result_files:
+                                result_file_path = results_files[result_file_name]
+                                new_file_name = f"{selected_model}_{video_name}_{result_file_name}"
+                                with open(result_file_path, "rb") as file:
+                                    zip_file.writestr(new_file_name, file.read())
+                        zip_buffer.seek(0)
+                        zip_file_name = f"results_{selected_model}_{video_name}.zip"
+                        st.download_button(
+                            label="Download Files",
+                            data=zip_buffer,
+                            file_name=zip_file_name,
+                            mime="application/zip"
+                        )
+            else:
+                st.write("No results files available for this video.")
+
+        if selected_video:
+            # read and show the predictions labeled video
+            video_file = open(selected_video, "rb")
+            video_bytes = video_file.read()
+            custom_css = """
+            <style>
+            video {
+                width: 75% !important;
+                height: 50% !important;
+            }
+            </style>
+            """
+            st.markdown(custom_css, unsafe_allow_html=True)
+            st.video(video_bytes)
+            st.caption(
+                "To download the video, click the three dots on the right side and select Download"
+            )
+        else:
+            st.write("No video to preview")
     else:
-        st.write("No video to preview")
+        # Show a basic UI of the page and an error with instractions 
+        st.header("Visualize Model Predictions")
+        st.error("To view a prediction video, you'll need to train a model and run inference first")
+        with st.sidebar:
+            st.write(" ")
+
